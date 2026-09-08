0b11a5f85a31ec1ed1c40180339b3bbde0fde601
Carlos Monroy <cmonroy66@gmail.com>
2026-09-07 22:21:42 -0400

    fix: corregir incidente detectado en el pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
         const fetchTasks = async () => { 
             const response = await fetch("http://localhost:3000/tasks"); 
             const data = await response.json(); setTasks(data); 
-        ; 
+        }; 
         fetchTasks(); 
     }, []);
 
